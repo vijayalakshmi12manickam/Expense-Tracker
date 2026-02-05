c482bdd6360ae53830369fd6f5923ab91b662003
Vijayalakshmi <vijayalakshmi0796@gmail.com>
2026-02-05 19:50:45 +0000

    Updated MongoDB URI

--- app/lib/mongodb.js
+++ app/lib/mongodb.js
@@ -1,25 +1,24 @@
 import mongoose from "mongoose";
 
-const MONGODB_URI = process.env.MONGODB_URI
-console.log("MONGODB_URI", MONGODB_URI)
+// const MONGODB_URI = process.env.MONGODB_URI
+const MONGODB_URI = process.env.VERCEL_MONGODB_URI;
+console.log("MONGODB_URI", MONGODB_URI);
 
-if(!MONGODB_URI){
-    throw new Error("Please define the mongoURI environemnt variable")
+if (!MONGODB_URI) {
+  throw new Error("Please define the mongoURI environemnt variable");
 }
 
 let cached = global.mongoose;
 
-if(!cached){
-    cached = global.mongoose = { conn: null, promise: null };
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
 }
 
 async function connectDB() {
   if (cached.conn) return cached.conn;
 
   if (!cached.promise) {
-    cached.promise = mongoose
-      .connect(MONGODB_URI)
-      .then((mongoose) => mongoose);
+    cached.promise = mongoose.connect(MONGODB_URI).then((mongoose) => mongoose);
   }
 
   cached.conn = await cached.promise;
